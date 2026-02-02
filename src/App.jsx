99251356971c9046a2d0ae02d34339d263e9d117
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-02-02 20:02:40 +0000

    uploaded logos

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
+        <img src="bwl_logo_lion.png" className="App-logo" alt="logo" />
+        <h1>
+          BWL Experiential Opportunities Board
+        </h1>
       </header>
     </div>
   );
