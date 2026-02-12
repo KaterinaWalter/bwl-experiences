b8c37279ae8e18ee53af48ef26cd1881bbefb82a
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2026-02-12 20:22:52 +0000

    header styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 export default function Header() {
     return (
         <header className="App-header container-fluid pt-3 text-center">
-            <nav class="navbar">
-                <div class="container-fluid">
-                    <div class="row">
-                    <a class="navbar-brand" href="#">
-                    <img src="bwl_lion_logo_square.png" alt="Logo" className="App-logo d-inline-block align-text-center"/>
-                    <span className="App-title">BWL Opportunities Board</span>
-                    <img src="bwl_lion_logo_square.png" alt="Logo" className="App-logo d-inline-block align-text-center"/>
-                    </a>
-                    <p className="App-description">Welcome, Lions! Here is your database for finding enrichment opportunities, including: <strong>extracurricular activities</strong>, <strong>summer programs</strong>, <strong>internships</strong>, <strong>jobs</strong>, and more.</p>
+
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-2">
+                            <img src="bwl_lion_logo_square.png" alt="Logo" className="App-logo d-inline-block"/>
+                        </div>
+                        <div className="col-8">
+                            <span className="App-title">BWL Opportunity Board</span>
+                        </div>
+                        <div className="col-2">
+                            <img src="bwl_lion_logo_square.png" alt="Logo" className="App-logo d-inline-block"/>
+                        </div>
+                    </div> 
+                    <div className="row">
+                        <p className="App-description">Welcome, Lions! Here is your database for finding enrichment opportunities, including: <strong>extracurricular activities</strong>, <strong>summer programs</strong>, <strong>internships</strong>, <strong>jobs</strong>, and more.</p>
                     </div>
-                    
                 </div>
-            </nav> 
             {/*
             <div className="d-flex justify-content-around">
                 <button className="btn btn-secondary shadow-sm fw-bold">Resumé Tips</button>
